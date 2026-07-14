c58edb010f66e413ebbeebf26e9ec3e25845f927
ruzanna <ruzanna.vardapetyan@advanced.am>
2026-07-14 18:48:41 +0400

    Added "_init" and "_destroy" functions.

--- include/airburst.h
+++ include/airburst.h
@@ -4,11 +4,17 @@
 #include <stdbool.h>
 
 bool airburst_init(void);
+void airburst_destroy(void);
 
+bool airburst_distance_init(void);
 float airburst_get_distance_m(void);
+bool airburst_distance_destroy(void);
+
+void airburst_test_init (void);
+bool airburst_test (void);
+void airburst_test_destroy(void);
+
 bool airburst_arm_bomb(void);
 bool airburst_bust_bomb(void);
-
-bool airburst_destroy(void);
 
 #endif //AIRBURST_H
--- src/airburst.c
+++ src/airburst.c
@@ -7,14 +7,17 @@
 #include "acc_hal_definitions.h"
 #include "acc_hal_integration.h"
 #include "acc_rss.h"
+#include "acc_rss_assembly_test.h"
 
+#define DEFAULT_SENSOR_ID 2
+#define EXAMPLE_HWAAS    (63)
 #define EXAMPLE_START_M  (0.02f)
 #define EXAMPLE_LENGTH_M (1.50f)
 #define EXAMPLE_PROFILE  ACC_SERVICE_PROFILE_2
-#define EXAMPLE_HWAAS    (63)
 
 bool success = true;
 acc_detector_distance_handle_t distance_handle;
+acc_rss_assembly_test_configuration_t configuration;
 
 static void set_config(acc_detector_distance_configuration_t distance_configuration) {
     acc_detector_distance_configuration_requested_start_set(distance_configuration, EXAMPLE_START_M);
@@ -23,18 +26,52 @@ static void set_config(acc_detector_distance_configuration_t distance_configurat
     acc_detector_distance_configuration_hw_accelerated_average_samples_set(distance_configuration, EXAMPLE_HWAAS);
 }
 
+static bool run_test(acc_rss_assembly_test_configuration_t configuration)
+{
+    acc_rss_assembly_test_result_t test_results[ACC_RSS_ASSEMBLY_TEST_MAX_NUMBER_OF_TESTS];
+    uint16_t                       nr_of_test_results = ACC_RSS_ASSEMBLY_TEST_MAX_NUMBER_OF_TESTS;
+    bool                           all_passed         = true;
+
+    if (!acc_rss_assembly_test(configuration, test_results, &nr_of_test_results))
+    {
+        printf("Airburst check test: Failed to complete\n");
+        return false;
+    }
+
+    for (uint16_t i = 0; i < nr_of_test_results; i++)
+    {
+        const bool passed = test_results[i].test_passed;
+        printf("Name: %s, result: %s\n", test_results[i].test_name, passed ? "Pass" : "Fail");
+
+        if (!passed)
+        {
+            all_passed = false;
+        }
+    }
+
+    return all_passed;
+}
+
 bool airburst_init(void) {
     const acc_hal_t *hal = acc_hal_integration_get_implementation();
 
     if (!acc_rss_activate(hal)) {
-        printf("acc_rss_activate() failed\n");
+        printf("airburst_init() failed\n");
         return false;
     }
 
+    return true;
+}
+
+void airburst_destroy(void) {
+    acc_rss_deactivate();
+}
+
+bool airburst_distance_init(void) {
     acc_detector_distance_configuration_t distance_configuration = acc_detector_distance_configuration_create();
 
     if (distance_configuration == NULL) {
-        printf("acc_detector_distance_configuration_create() failed\n");
+        printf("airburst_distance_init() failed\n");
         acc_rss_deactivate();
         return false;
     }
@@ -76,25 +113,48 @@ float airburst_get_distance_m(void) {
     return result[0].distance_m;
 }
 
-bool airburst_arm_bomb(void) {
-    return true;
-}
-
-bool airburst_bust_bomb(void) {
-    return true;
-}
-
-bool airburst_destroy(void) {
+bool airburst_distance_destroy(void) {
     bool deactivated = acc_detector_distance_deactivate(distance_handle);
 
     acc_detector_distance_destroy(&distance_handle);
-
-    acc_rss_deactivate();
 
     if (deactivated && success) {
         printf("Application finished OK\n");
         return false;
     }
 
+    return true;
+}
+
+void airburst_test_init(void) {
+    configuration = acc_rss_assembly_test_configuration_create();
+    acc_rss_assembly_test_configuration_sensor_set(configuration, DEFAULT_SENSOR_ID);
+
+    // Disable all tests (they are enabled by default)
+    acc_rss_assembly_test_configuration_all_tests_disable(configuration);
+
+    // Enable and run: write Read Test
+    acc_rss_assembly_test_configuration_communication_write_read_test_enable(configuration);
+}
+
+bool airburst_test (void) {
+    if (!run_test(configuration))
+    {
+        printf("Airbust check test: Write Read Test failed\n");
+        acc_rss_assembly_test_configuration_destroy(&configuration);
+        return true;
+    }
+    return false;
+}
+
+void airburst_test_destroy(void) {
+    acc_rss_assembly_test_configuration_communication_write_read_test_disable(configuration);
+}
+
+bool airburst_arm_bomb(void) {
+    return true;
+}
+
+bool airburst_bust_bomb(void) {
     return true;
 }
